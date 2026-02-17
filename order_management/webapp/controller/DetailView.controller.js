05e7395f02fd807391a46fb7923fd88e79616728
Mariel Tatel <mariel.tatel@accenture.com>
2026-02-17 23:00:30 +0000

    Added Detail Page

--- order_management/webapp/controller/DetailView.controller.js
+++ order_management/webapp/controller/DetailView.controller.js
@@ -1,6 +1,9 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/core/routing/History"
+], (Controller, Filter, FilterOperator, History) => {
     "use strict";
 
     return Controller.extend("sapips.training.ordermanagement.controller.DetailView", {
@@ -9,6 +12,38 @@ sap.ui.define([
                 .getRouter()
                 .getRoute("RouteDetailPage")
                 .attachPatternMatched(this._onMatched, this);
+        },
+
+        onProductsUpdateFinished: function (oEvent) {
+            var oTable = oEvent.getSource();
+            var iTotalItems = oTable.getBinding("items").getLength();
+
+            if (typeof iTotalItems === "number") {
+                oTable.setHeaderText("Product (" + iTotalItems + ")");
+            } 
+
+        },
+
+        onPressEdit: function () {
+            var sOrderId = this._orderId;
+
+            var oRouter = this.getOwnerComponent().getRouter();
+
+            oRouter.navTo("RouteEditPage", {
+                orderID: encodeURIComponent(sOrderId)
+            });
+        },
+
+        onPressCancel: function () {
+            var oHistory = History.getInstance();
+            var sPreviousHash = oHistory.getPreviousHash();
+            var oRouter = this.getOwnerComponent().getRouter();
+
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1);
+            } else {
+                oRouter.navTo("RouteOrderMainView", {}, true);
+            }
         },
 
         _onMatched: function (oEvent) {
@@ -20,10 +55,25 @@ sap.ui.define([
             oForm.bindElement({
                 path: sPath,
                 parameters: {
-                expand: "ToReceivingPlant,ToDeliveringPlant"
-                }
+                    expand: "ToReceivingPlant,ToDeliveringPlant"
+                    }
             });
-            
+
+            this._orderId = sOrderId;
+            this._filterProductsByOrderID(sOrderId);
+        },
+        
+        _filterProductsByOrderID: function (sOrderId) {
+            var oTable = this.getView().byId("productOrderTable");
+
+            if (sOrderId) {
+                var aFilter = [
+                    new Filter("OrderID", FilterOperator.EQ, sOrderId)
+                ];
+            }
+
+            var oBindingContext = oTable.getBinding("items");
+            oBindingContext.filter(aFilter);
         }
     });
 });
